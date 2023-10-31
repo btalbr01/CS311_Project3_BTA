da6650c02af9f5fc5d2da5085f9e9ec821311317
btalbr01 <136751899+btalbr01@users.noreply.github.com>
2023-10-30 22:32:03 -0400

    Add files via upload
    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,461 @@
+﻿namespace CS311_Project3_BTA
+{
+    partial class frmBensPizzaPlace
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBensPizzaPlace));
+            pictureBox1 = new PictureBox();
+            lblNensPizzaPlace = new Label();
+            groupBox1 = new GroupBox();
+            ckbMushroom = new CheckBox();
+            ckbExtraCheese = new CheckBox();
+            ckbJalapeno = new CheckBox();
+            ckbBananaPeppers = new CheckBox();
+            ckbBlackOlives = new CheckBox();
+            ckbGreenOlives = new CheckBox();
+            ckbGreenPepper = new CheckBox();
+            ckbOnion = new CheckBox();
+            ckbSpicyItalian = new CheckBox();
+            ckbCanadianBacon = new CheckBox();
+            ckbSausage = new CheckBox();
+            ckbPepperoni = new CheckBox();
+            lblToppings = new Label();
+            rdoRegular = new RadioButton();
+            rdoThick = new RadioButton();
+            rdoThin = new RadioButton();
+            lblCrust = new Label();
+            cboSizes = new ComboBox();
+            lblSize = new Label();
+            rtfOrderSummary = new RichTextBox();
+            lblSubTotal = new Label();
+            lblTax = new Label();
+            lblTotal = new Label();
+            btnCalculate = new Button();
+            lblOrderSummary = new Label();
+            txtSubtotal = new TextBox();
+            txtTax = new TextBox();
+            txtTotal = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(200, 89);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // lblNensPizzaPlace
+            // 
+            lblNensPizzaPlace.AutoSize = true;
+            lblNensPizzaPlace.Font = new Font("Segoe UI", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            lblNensPizzaPlace.Location = new Point(218, 12);
+            lblNensPizzaPlace.Name = "lblNensPizzaPlace";
+            lblNensPizzaPlace.Size = new Size(538, 86);
+            lblNensPizzaPlace.TabIndex = 1;
+            lblNensPizzaPlace.Text = "Ben's Pizza Place";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(ckbMushroom);
+            groupBox1.Controls.Add(ckbExtraCheese);
+            groupBox1.Controls.Add(ckbJalapeno);
+            groupBox1.Controls.Add(ckbBananaPeppers);
+            groupBox1.Controls.Add(ckbBlackOlives);
+            groupBox1.Controls.Add(ckbGreenOlives);
+            groupBox1.Controls.Add(ckbGreenPepper);
+            groupBox1.Controls.Add(ckbOnion);
+            groupBox1.Controls.Add(ckbSpicyItalian);
+            groupBox1.Controls.Add(ckbCanadianBacon);
+            groupBox1.Controls.Add(ckbSausage);
+            groupBox1.Controls.Add(ckbPepperoni);
+            groupBox1.Controls.Add(lblToppings);
+            groupBox1.Controls.Add(rdoRegular);
+            groupBox1.Controls.Add(rdoThick);
+            groupBox1.Controls.Add(rdoThin);
+            groupBox1.Controls.Add(lblCrust);
+            groupBox1.Controls.Add(cboSizes);
+            groupBox1.Controls.Add(lblSize);
+            groupBox1.Location = new Point(12, 107);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(776, 186);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            // 
+            // ckbMushroom
+            // 
+            ckbMushroom.AutoSize = true;
+            ckbMushroom.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            ckbMushroom.Location = new Point(301, 136);
+            ckbMushroom.Name = "ckbMushroom";
+            ckbMushroom.Size = new Size(86, 19);
+            ckbMushroom.TabIndex = 18;
+            ckbMushroom.Text = "Mushroom";
+            ckbMushroom.UseVisualStyleBackColor = true;
+            // 
+            // ckbExtraCheese
+            // 
+            ckbExtraCheese.AutoSize = true;
+            ckbExtraCheese.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            ckbExtraCheese.Location = new Point(302, 113);
+            ckbExtraCheese.Name = "ckbExtraCheese";
+            ckbExtraCheese.Size = new Size(98, 19);
+            ckbExtraCheese.TabIndex = 17;
+            ckbExtraCheese.Text = "Extra Cheese";
+            ckbExtraCheese.UseVisualStyleBackColor = true;
+            // 
+            // ckbJalapeno
+            // 
+            ckbJalapeno.AutoSize = true;
+            ckbJalapeno.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            ckbJalapeno.Location = new Point(302, 89);
+            ckbJalapeno.Name = "ckbJalapeno";
+            ckbJalapeno.Size = new Size(74, 19);
+            ckbJalapeno.TabIndex = 16;
+            ckbJalapeno.Text = "Jalapeno";
+            ckbJalapeno.UseVisualStyleBackColor = true;
+            // 
+            // ckbBananaPeppers
+            // 
+            ckbBananaPeppers.AutoSize = true;
+            ckbBananaPeppers.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            ckbBananaPeppers.Location = new Point(301, 63);
+            ckbBananaPeppers.Name = "ckbBananaPeppers";
+            ckbBananaPeppers.Size = new Size(114, 19);
+            ckbBananaPeppers.TabIndex = 15;
+            ckbBananaPeppers.Text = "Banana Peppers";
+            ckbBananaPeppers.UseVisualStyleBackColor = true;
+            // 
+            // ckbBlackOlives
+            // 
+            ckbBlackOlives.AutoSize = true;
+            ckbBlackOlives.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            ckbBlackOlives.Location = new Point(183, 113);
+            ckbBlackOlives.Name = "ckbBlackOlives";
+            ckbBlackOlives.Size = new Size(93, 19);
+            ckbBlackOlives.TabIndex = 14;
+            ckbBlackOlives.Text = "Black Olives";
+            ckbBlackOlives.UseVisualStyleBackColor = true;
+            // 
+            // ckbGreenOlives
+            // 
+            ckbGreenOlives.AutoSize = true;
+            ckbGreenOlives.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            ckbGreenOlives.Location = new Point(183, 138);
+            ckbGreenOlives.Name = "ckbGreenOlives";
+            ckbGreenOlives.Size = new Size(98, 19);
+            ckbGreenOlives.TabIndex = 13;
+            ckbGreenOlives.Text = "Green Olives";
+            ckbGreenOlives.UseVisualStyleBackColor = true;
+            // 
+            // ckbGreenPepper
+            // 
+            ckbGreenPepper.AutoSize = true;
+            ckbGreenPepper.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            ckbGreenPepper.Location = new Point(183, 88);
+            ckbGreenPepper.Name = "ckbGreenPepper";
+            ckbGreenPepper.Size = new Size(104, 19);
+            ckbGreenPepper.TabIndex = 12;
+            ckbGreenPepper.Text = "Green Pepper";
+            ckbGreenPepper.UseVisualStyleBackColor = true;
+            // 
+            // ckbOnion
+            // 
+            ckbOnion.AutoSize = true;
+            ckbOnion.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            ckbOnion.Location = new Point(183, 63);
+            ckbOnion.Name = "ckbOnion";
+            ckbOnion.Size = new Size(59, 19);
+            ckbOnion.TabIndex = 11;
+            ckbOnion.Text = "Onion";
+            ckbOnion.UseVisualStyleBackColor = true;
+            // 
+            // ckbSpicyItalian
+            // 
+            ckbSpicyItalian.AutoSize = true;
+            ckbSpicyItalian.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            ckbSpicyItalian.Location = new Point(42, 136);
+            ckbSpicyItalian.Name = "ckbSpicyItalian";
+            ckbSpicyItalian.Size = new Size(140, 19);
+            ckbSpicyItalian.TabIndex = 10;
+            ckbSpicyItalian.Text = "Spicy Italian Sausage";
+            ckbSpicyItalian.UseVisualStyleBackColor = true;
+            // 
+            // ckbCanadianBacon
+            // 
+            ckbCanadianBacon.AutoSize = true;
+            ckbCanadianBacon.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            ckbCanadianBacon.Location = new Point(42, 111);
+            ckbCanadianBacon.Name = "ckbCanadianBacon";
+            ckbCanadianBacon.Size = new Size(112, 19);
+            ckbCanadianBacon.TabIndex = 9;
+            ckbCanadianBacon.Text = "Canadian Bacon";
+            ckbCanadianBacon.UseVisualStyleBackColor = true;
+            // 
+            // ckbSausage
+            // 
+            ckbSausage.AutoSize = true;
+            ckbSausage.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            ckbSausage.Location = new Point(42, 86);
+            ckbSausage.Name = "ckbSausage";
+            ckbSausage.Size = new Size(71, 19);
+            ckbSausage.TabIndex = 8;
+            ckbSausage.Text = "Sausage";
+            ckbSausage.UseVisualStyleBackColor = true;
+            // 
+            // ckbPepperoni
+            // 
+            ckbPepperoni.AutoSize = true;
+            ckbPepperoni.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            ckbPepperoni.ForeColor = SystemColors.ControlText;
+            ckbPepperoni.Location = new Point(43, 65);
+            ckbPepperoni.Name = "ckbPepperoni";
+            ckbPepperoni.Size = new Size(83, 19);
+            ckbPepperoni.TabIndex = 7;
+            ckbPepperoni.Text = "Pepperoni";
+            ckbPepperoni.UseVisualStyleBackColor = true;
+            // 
+            // lblToppings
+            // 
+            lblToppings.AutoSize = true;
+            lblToppings.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lblToppings.Location = new Point(32, 47);
+            lblToppings.Name = "lblToppings";
+            lblToppings.Size = new Size(59, 15);
+            lblToppings.TabIndex = 6;
+            lblToppings.Text = "Toppings:";
+            // 
+            // rdoRegular
+            // 
+            rdoRegular.AutoSize = true;
+            rdoRegular.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            rdoRegular.Location = new Point(423, 17);
+            rdoRegular.Name = "rdoRegular";
+            rdoRegular.Size = new Size(68, 19);
+            rdoRegular.TabIndex = 5;
+            rdoRegular.TabStop = true;
+            rdoRegular.Text = "Regular";
+            rdoRegular.UseVisualStyleBackColor = true;
+            // 
+            // rdoThick
+            // 
+            rdoThick.AutoSize = true;
+            rdoThick.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            rdoThick.Location = new Point(362, 17);
+            rdoThick.Name = "rdoThick";
+            rdoThick.Size = new Size(55, 19);
+            rdoThick.TabIndex = 4;
+            rdoThick.TabStop = true;
+            rdoThick.Text = "Thick";
+            rdoThick.UseVisualStyleBackColor = true;
+            // 
+            // rdoThin
+            // 
+            rdoThin.AutoSize = true;
+            rdoThin.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            rdoThin.Location = new Point(307, 17);
+            rdoThin.Name = "rdoThin";
+            rdoThin.Size = new Size(49, 19);
+            rdoThin.TabIndex = 3;
+            rdoThin.TabStop = true;
+            rdoThin.Text = "Thin";
+            rdoThin.UseVisualStyleBackColor = true;
+            // 
+            // lblCrust
+            // 
+            lblCrust.AutoSize = true;
+            lblCrust.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lblCrust.Location = new Point(233, 19);
+            lblCrust.Name = "lblCrust";
+            lblCrust.Size = new Size(68, 15);
+            lblCrust.TabIndex = 2;
+            lblCrust.Text = "Crust Type:";
+            // 
+            // cboSizes
+            // 
+            cboSizes.FormattingEnabled = true;
+            cboSizes.Items.AddRange(new object[] { "Small", "Medium", "Large", "X-Large", "Ginormous" });
+            cboSizes.Location = new Point(97, 11);
+            cboSizes.Name = "cboSizes";
+            cboSizes.Size = new Size(121, 23);
+            cboSizes.TabIndex = 1;
+            // 
+            // lblSize
+            // 
+            lblSize.AutoSize = true;
+            lblSize.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lblSize.Location = new Point(58, 19);
+            lblSize.Name = "lblSize";
+            lblSize.Size = new Size(33, 15);
+            lblSize.TabIndex = 0;
+            lblSize.Text = "Size:";
+            // 
+            // rtfOrderSummary
+            // 
+            rtfOrderSummary.Location = new Point(117, 299);
+            rtfOrderSummary.Name = "rtfOrderSummary";
+            rtfOrderSummary.Size = new Size(436, 143);
+            rtfOrderSummary.TabIndex = 3;
+            rtfOrderSummary.Text = "";
+            // 
+            // lblSubTotal
+            // 
+            lblSubTotal.AutoSize = true;
+            lblSubTotal.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lblSubTotal.Location = new Point(592, 299);
+            lblSubTotal.Name = "lblSubTotal";
+            lblSubTotal.Size = new Size(58, 15);
+            lblSubTotal.TabIndex = 4;
+            lblSubTotal.Text = "SubTotal:";
+            // 
+            // lblTax
+            // 
+            lblTax.AutoSize = true;
+            lblTax.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTax.Location = new Point(621, 324);
+            lblTax.Name = "lblTax";
+            lblTax.Size = new Size(29, 15);
+            lblTax.TabIndex = 5;
+            lblTax.Text = "Tax:";
+            // 
+            // lblTotal
+            // 
+            lblTotal.AutoSize = true;
+            lblTotal.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTotal.Location = new Point(613, 348);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(37, 15);
+            lblTotal.TabIndex = 6;
+            lblTotal.Text = "Total:";
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btnCalculate.Location = new Point(559, 369);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(197, 57);
+            btnCalculate.TabIndex = 7;
+            btnCalculate.Text = "Calculate";
+            btnCalculate.UseVisualStyleBackColor = true;
+            btnCalculate.Click += btnCalculate_Click;
+            // 
+            // lblOrderSummary
+            // 
+            lblOrderSummary.AutoSize = true;
+            lblOrderSummary.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lblOrderSummary.Location = new Point(12, 299);
+            lblOrderSummary.Name = "lblOrderSummary";
+            lblOrderSummary.Size = new Size(99, 15);
+            lblOrderSummary.TabIndex = 8;
+            lblOrderSummary.Text = "Order Summary:";
+            // 
+            // txtSubtotal
+            // 
+            txtSubtotal.Location = new Point(656, 291);
+            txtSubtotal.Name = "txtSubtotal";
+            txtSubtotal.Size = new Size(100, 23);
+            txtSubtotal.TabIndex = 9;
+            // 
+            // txtTax
+            // 
+            txtTax.Location = new Point(656, 316);
+            txtTax.Name = "txtTax";
+            txtTax.Size = new Size(100, 23);
+            txtTax.TabIndex = 10;
+            // 
+            // txtTotal
+            // 
+            txtTotal.Location = new Point(656, 340);
+            txtTotal.Name = "txtTotal";
+            txtTotal.Size = new Size(100, 23);
+            txtTotal.TabIndex = 11;
+            // 
+            // frmBensPizzaPlace
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtTotal);
+            Controls.Add(txtTax);
+            Controls.Add(txtSubtotal);
+            Controls.Add(lblOrderSummary);
+            Controls.Add(btnCalculate);
+            Controls.Add(lblTotal);
+            Controls.Add(lblTax);
+            Controls.Add(lblSubTotal);
+            Controls.Add(rtfOrderSummary);
+            Controls.Add(groupBox1);
+            Controls.Add(lblNensPizzaPlace);
+            Controls.Add(pictureBox1);
+            Name = "frmBensPizzaPlace";
+            Text = "Ben's Pizza Place";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label lblNensPizzaPlace;
+        private GroupBox groupBox1;
+        private RadioButton rdoRegular;
+        private RadioButton rdoThick;
+        private RadioButton rdoThin;
+        private Label lblCrust;
+        private ComboBox cboSizes;
+        private Label lblSize;
+        private CheckBox ckbMushroom;
+        private CheckBox ckbExtraCheese;
+        private CheckBox ckbJalapeno;
+        private CheckBox ckbBananaPeppers;
+        private CheckBox ckbBlackOlives;
+        private CheckBox ckbGreenOlives;
+        private CheckBox ckbGreenPepper;
+        private CheckBox ckbOnion;
+        private CheckBox ckbSpicyItalian;
+        private CheckBox ckbCanadianBacon;
+        private CheckBox ckbSausage;
+        private CheckBox ckbPepperoni;
+        private Label lblToppings;
+        private RichTextBox rtfOrderSummary;
+        private Label lblSubTotal;
+        private Label lblTax;
+        private Label lblTotal;
+        private Button btnCalculate;
+        private Label lblOrderSummary;
+        private TextBox txtSubtotal;
+        private TextBox txtTax;
+        private TextBox txtTotal;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+using System.DirectoryServices.ActiveDirectory;
+
+namespace CS311_Project3_BTA
+{
+    public partial class frmBensPizzaPlace : Form
+    {
+        double tax = 1.06;
+
+
+        public frmBensPizzaPlace()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            Summarize();
+        }
+
+        private void Summarize()
+        {
+            float subtotal = 0;
+            double tax;
+            double total;
+            String size = cboSizes.GetItemText(cboSizes.SelectedItem);
+            String crust = "";
+            rtfOrderSummary.Text = "";
+            if (cboSizes.Text == "Small")
+                subtotal = subtotal + 2;
+            if (cboSizes.Text == "Medium")
+                subtotal = subtotal + 5;
+            if (cboSizes.Text == "Large")
+                subtotal = subtotal + 10;
+            if (cboSizes.Text == "X-Large")
+                subtotal = subtotal + 15;
+            if (cboSizes.Text == "Ginormous")
+                subtotal = subtotal + 20;
+            if (ckbPepperoni.Checked == true)
+                subtotal = subtotal + 2;
+            if (ckbSausage.Checked == true)
+                subtotal = subtotal + 2;
+            if (ckbCanadianBacon.Checked == true)
+                subtotal = subtotal + 2;
+            if (ckbSpicyItalian.Checked == true)
+                subtotal = subtotal + 2;
+            if (ckbOnion.Checked == true)
+                subtotal = subtotal + 1;
+            if (ckbGreenPepper.Checked == true)
+                subtotal = subtotal + 1;
+            if (ckbBlackOlives.Checked == true)
+                subtotal = subtotal + 1;
+            if (ckbGreenOlives.Checked == true)
+                subtotal = subtotal + 1;
+            if (ckbBananaPeppers.Checked == true)
+                subtotal = subtotal + 1;
+            if (ckbJalapeno.Checked == true)
+                subtotal = subtotal + 1;
+            if (ckbExtraCheese.Checked == true)
+                subtotal = subtotal + 1;
+            if (ckbMushroom.Checked == true)
+                subtotal = subtotal + 1;
+
+            if (rdoThin.Checked)
+                crust = "Thin";
+            else if (rdoThick.Checked)
+                crust = "Thick";
+            else if (rdoRegular.Checked)
+                crust = "Regular";
+
+            if (crust == "" || size == "")
+                rtfOrderSummary.Text = "Please choose a size or crust";
+
+            else
+            {
+                txtSubtotal.Text = subtotal.ToString("C");
+                tax = subtotal * 0.06;
+                txtTax.Text = tax.ToString("C");
+                total = subtotal + tax;
+                txtTotal.Text = total.ToString("C");
+                rtfOrderSummary.Text = "You ordered a " + size + " pizza with " + crust +
+                    " crust and the following toppings:\n";
+            }
+
+            rtfOrderSummary.SelectionBullet = true;
+
+            if (ckbPepperoni.Checked == true)
+                rtfOrderSummary.SelectedText = "Pepperoni\n";
+            if (ckbSausage.Checked == true)
+                rtfOrderSummary.SelectedText = "Sausage\n";
+            if (ckbCanadianBacon.Checked == true)
+                rtfOrderSummary.SelectedText = "Canadian Bacon\n";
+            if (ckbSpicyItalian.Checked == true)
+                rtfOrderSummary.SelectedText = "Spicy Italian Sausage\n";
+            if (ckbOnion.Checked == true)
+                rtfOrderSummary.SelectedText = "Onion\n";
+            if (ckbGreenPepper.Checked == true)
+                rtfOrderSummary.SelectedText = "Green Pepper\n";
+            if (ckbBlackOlives.Checked == true)
+                rtfOrderSummary.SelectedText = "Black Olives\n";
+            if (ckbGreenOlives.Checked == true)
+                rtfOrderSummary.SelectedText = "Green Olives\n";
+            if (ckbBananaPeppers.Checked == true)
+                rtfOrderSummary.SelectedText = "Banana Peppers\n";
+            if (ckbJalapeno.Checked == true)
+                rtfOrderSummary.SelectedText = "Jalapeno\n";
+            if (ckbExtraCheese.Checked == true)
+                rtfOrderSummary.SelectedText  = "Extra Cheese\n";
+            if (ckbMushroom.Checked == true)
+                rtfOrderSummary.SelectedText  = "Mushroom\n";
+
+            rtfOrderSummary.SelectionBullet = false;
+        }    
+    }
+}
