3c4039bfb8d7bce580b75b2d6ba2ea0e7bff94f8
btalbr01 <136751899+btalbr01@users.noreply.github.com>
2023-10-31 08:51:18 -0400

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -35,54 +35,54 @@ namespace CS311_Project3_BTA
 
             //if statements to determine pizza size and cost for that size.
             if (cboSizes.Text == "Small")
-                subtotal = subtotal + 2;
+                subtotal = subtotal + 2; //end if
             if (cboSizes.Text == "Medium")
-                subtotal = subtotal + 5;
+                subtotal = subtotal + 5; //end if
             if (cboSizes.Text == "Large")
-                subtotal = subtotal + 10;
+                subtotal = subtotal + 10; //end if
             if (cboSizes.Text == "X-Large")
-                subtotal = subtotal + 15;
+                subtotal = subtotal + 15; //end if
             if (cboSizes.Text == "Ginormous")
-                subtotal = subtotal + 20;
+                subtotal = subtotal + 20; //end if
 
             //if statements to determine what toppings are on the pizza and 
             //add their cost to the subtotal.
             if (ckbPepperoni.Checked == true)
-                subtotal = subtotal + 2;
+                subtotal = subtotal + 2; //end if
             if (ckbSausage.Checked == true)
-                subtotal = subtotal + 2;
+                subtotal = subtotal + 2; //end if
             if (ckbCanadianBacon.Checked == true)
-                subtotal = subtotal + 2;
+                subtotal = subtotal + 2; //end if
             if (ckbSpicyItalian.Checked == true)
-                subtotal = subtotal + 2;
+                subtotal = subtotal + 2; //end if
             if (ckbOnion.Checked == true)
-                subtotal = subtotal + 1;
+                subtotal = subtotal + 1; //end if
             if (ckbGreenPepper.Checked == true)
-                subtotal = subtotal + 1;
+                subtotal = subtotal + 1; //end if
             if (ckbBlackOlives.Checked == true)
-                subtotal = subtotal + 1;
+                subtotal = subtotal + 1; //end if
             if (ckbGreenOlives.Checked == true)
-                subtotal = subtotal + 1;
+                subtotal = subtotal + 1; //end if
             if (ckbBananaPeppers.Checked == true)
-                subtotal = subtotal + 1;
+                subtotal = subtotal + 1; //end if
             if (ckbJalapeno.Checked == true)
-                subtotal = subtotal + 1;
+                subtotal = subtotal + 1; //end if
             if (ckbExtraCheese.Checked == true)
-                subtotal = subtotal + 1;
+                subtotal = subtotal + 1; //end if
             if (ckbMushroom.Checked == true)
-                subtotal = subtotal + 1;
+                subtotal = subtotal + 1; //end if
 
             //sets crust variable to crust type picked by user.
             if (rdoThin.Checked)
-                crust = "Thin";
+                crust = "Thin"; //end if
             else if (rdoThick.Checked)
-                crust = "Thick";
+                crust = "Thick"; //end if
             else if (rdoRegular.Checked)
-                crust = "Regular";
+                crust = "Regular"; //end if
 
             //if crust type or size not chosen, prompts user to choose them.
             if (crust == "" || size == "")
-                rtfOrderSummary.Text = "Please choose a size or crust";
+                rtfOrderSummary.Text = "Please choose a size or crust"; //end if
 
             //if crust and size chosen, prints subtotal, tax and total to
             //their text boxes as dollar amounts.
@@ -103,29 +103,29 @@ namespace CS311_Project3_BTA
             //if statements to determine what topings were chosen
             //and display them as bulleted points.
             if (ckbPepperoni.Checked == true)
-                rtfOrderSummary.SelectedText = "Pepperoni\n";
+                rtfOrderSummary.SelectedText = "Pepperoni\n"; //end if
             if (ckbSausage.Checked == true)
-                rtfOrderSummary.SelectedText = "Sausage\n";
+                rtfOrderSummary.SelectedText = "Sausage\n"; //end if
             if (ckbCanadianBacon.Checked == true)
-                rtfOrderSummary.SelectedText = "Canadian Bacon\n";
+                rtfOrderSummary.SelectedText = "Canadian Bacon\n"; //end if
             if (ckbSpicyItalian.Checked == true)
-                rtfOrderSummary.SelectedText = "Spicy Italian Sausage\n";
+                rtfOrderSummary.SelectedText = "Spicy Italian Sausage\n"; //end if
             if (ckbOnion.Checked == true)
-                rtfOrderSummary.SelectedText = "Onion\n";
+                rtfOrderSummary.SelectedText = "Onion\n"; //end if
             if (ckbGreenPepper.Checked == true)
-                rtfOrderSummary.SelectedText = "Green Pepper\n";
+                rtfOrderSummary.SelectedText = "Green Pepper\n"; //end if
             if (ckbBlackOlives.Checked == true)
-                rtfOrderSummary.SelectedText = "Black Olives\n";
+                rtfOrderSummary.SelectedText = "Black Olives\n"; //end if
             if (ckbGreenOlives.Checked == true)
-                rtfOrderSummary.SelectedText = "Green Olives\n";
+                rtfOrderSummary.SelectedText = "Green Olives\n"; //end if
             if (ckbBananaPeppers.Checked == true)
-                rtfOrderSummary.SelectedText = "Banana Peppers\n";
+                rtfOrderSummary.SelectedText = "Banana Peppers\n"; //end if
             if (ckbJalapeno.Checked == true)
-                rtfOrderSummary.SelectedText = "Jalapeno\n";
+                rtfOrderSummary.SelectedText = "Jalapeno\n"; //end if
             if (ckbExtraCheese.Checked == true)
-                rtfOrderSummary.SelectedText = "Extra Cheese\n";
+                rtfOrderSummary.SelectedText = "Extra Cheese\n"; //end if
             if (ckbMushroom.Checked == true)
-                rtfOrderSummary.SelectedText = "Mushroom\n";
+                rtfOrderSummary.SelectedText = "Mushroom\n"; //end if
 
             //ends bullet point formatting.
             rtfOrderSummary.SelectionBullet = false;
